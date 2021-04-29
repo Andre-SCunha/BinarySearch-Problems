3420c0d428002a783f7f512ddba882a5846b65ab
Andre-SCunha <brayils.andre@gmail.com>
2021-04-29 11:12:35 -0300

    Add/Easy Problems

---
+++ Easy/153.js
@@ -0,0 +1,12 @@
+class Solution {
+    solve(nums) {
+        const sortedNums = nums.slice(0).sort((a, b) => a - b);
+        let count = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === sortedNums[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
